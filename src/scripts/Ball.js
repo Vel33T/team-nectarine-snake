19ca2beeb81c19e31937b23dbae185af53f993b5
vlvsv87 <vlvsv87@gmail.com>
2013-06-05 20:47:55 +0000

    Minor refactoring, just to try commiting

--- src/scripts/Ball.js
+++ src/scripts/Ball.js
@@ -1,14 +1,14 @@
 define(function(require) {
     'use strict';
 
-    var utils = require('utils')
-    var MovingObject = require('MovingObject')
+    var utils = require('utils');
+    var MovingObject = require('MovingObject');
 
     function Ball(position, direction) {
-        MovingObject.call(this, [[ true ]], position, '#999', direction)
+        MovingObject.call(this, [[true]], position, '#999', direction);
     }
 
-    utils.inherit(Ball, MovingObject)
+    utils.inherit(Ball, MovingObject);
 
-    return Ball
+    return Ball;
 })
