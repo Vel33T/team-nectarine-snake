c4fae552587ad347f536b1e92ed882cc256df07f
vladislavdd@gmail.com <vladislavdd@gmail.com@5ec6e21a-d4b0-5386-4258-d97f8457d6a6>
2013-06-16 11:37:06 +0000

    Debugging - Repaired

--- trunk/SnakeGame/Snake/scripts-tests/point-tests.js
+++ trunk/SnakeGame/Snake/scripts-tests/point-tests.js
@@ -1,8 +1,8 @@
 ﻿/// <reference path="../src/scripts/MovingObject.js" />
 /// <reference path="../QUnit/qunit-1.11.0.js" />
 define(function (require) {
-    var Point = require('scripts/Point.js');
-    var MovingObject = require('scripts/MovingObject.js');
+    var Point = require('../scripts/Point.js');
+    var MovingObject = require('../scripts/MovingObject.js');
 
     var obj = new MovingObject(null, Point(5, 5), null, Point(1, 1));
 
@@ -12,7 +12,8 @@ define(function (require) {
             obj.update();
 
             var expected = Point(6, 6);
-            deepEqual(obj.position, expected);
+            deepEqual(obj.position.row, expected.row);
+            deepEqual(obj.position.col, expected.col);
         });
 });
 
