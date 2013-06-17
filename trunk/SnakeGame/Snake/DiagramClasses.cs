d1ae19f4af3df7f5d29452daeff1d8e2eca4329e
Antonia.Stoeva@gmail.com <Antonia.Stoeva@gmail.com@5ec6e21a-d4b0-5386-4258-d97f8457d6a6>
2013-06-17 07:05:47 +0000

    Added class diagram and some classes in it.

---
+++ trunk/SnakeGame/Snake/DiagramClasses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Snake
+{
+    public class DiagramClasses
+    {
+    }
+
+    public class Ball
+    {
+    }
+
+    public class Block
+    {
+    }
+
+    public class Food
+    {
+    }
+
+    public class Snake
+    {
+    }
+
+    public class Point
+    {
+    }
+
+    public abstract class GameObject
+    {
+    }
+
+    public abstract class MovingObject
+    {
+    }
+}
